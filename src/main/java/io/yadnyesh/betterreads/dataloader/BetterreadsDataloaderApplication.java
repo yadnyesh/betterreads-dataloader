6d86841534ef8633b102de778be3b50f4a98158c
yadnyesh <yadnyesh.juvekar@gmail.com>
2021-12-29 14:02:50 +0530

    First Commit

---
+++ src/main/java/io/yadnyesh/betterreads/dataloader/BetterreadsDataloaderApplication.java
@@ -0,0 +1,13 @@
+package io.yadnyesh.betterreads.dataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterreadsDataloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterreadsDataloaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/yadnyesh/betterreads/dataloader/BetterreadsDataloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yadnyesh.betterreads.dataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterreadsDataloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
